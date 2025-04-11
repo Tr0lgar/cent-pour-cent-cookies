53b7ad614d4b021cf3967a72083b8ee5e12f4e74
Mathis <mathis.derwael@gmail.com>
2025-04-11 13:38:19 +0200

    clean index start

--- src/app/shared/components/layout/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/layout/nav-bar/nav-bar.component.ts
@@ -1,10 +1,10 @@
-import {Component, input} from '@angular/core';
+import {Component, HostListener, input} from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {LinkModel} from './models/link.model';
 import { faCookieBite } from '@fortawesome/free-solid-svg-icons';
 import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
-import {NgIf} from '@angular/common';
+import {NgClass, NgIf, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
@@ -12,15 +12,39 @@ import {NgIf} from '@angular/common';
     RouterLink,
     RouterLinkActive,
     FaIconComponent,
-    NgIf
+    NgIf,
+    NgStyle
   ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
+  // Fontawesome Icons
   faCookieBite = faCookieBite;
   faFacebook = faFacebook;
   faInstagram = faInstagram;
+
+  bgColor: string = 'rgba(0, 0, 0, 0.3)';
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition = window.scrollY ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop || 0;
+    const clientHeight = document.documentElement.clientHeight;
+
+    const factor = Math.min(scrollPosition / (clientHeight - (clientHeight * 15 / 100)), 1);
+
+    const initial = { r: 50, g: 50, b: 50, a: 0.3 };
+    const final = { r: 139, g: 86, b: 55, a: 1 };
+
+    const r = Math.round(initial.r + (final.r - initial.r) * factor);
+    const g = Math.round(initial.g + (final.g - initial.g) * factor);
+    const b = Math.round(initial.b + (final.b - initial.b) * factor);
+    const a = Number((initial.a + (final.a - initial.a) * factor).toFixed(2));
+
+    this.bgColor = `rgba(${r}, ${g}, ${b}, ${a})`;
+  }
 
   links = input.required<LinkModel[]>();
 }
